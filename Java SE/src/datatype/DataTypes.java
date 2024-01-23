60573b6a1d90223edbae0aa3112772335af09c9d
tipu721 <tsultan721@gmail.com>
2024-01-23 22:38:58 +0600

    data type and operator

---
+++ Java SE/src/datatype/DataTypes.java
@@ -0,0 +1,16 @@
+package datatype;
+
+public class DataTypes {
+
+    // primitive data type
+    boolean bol = true;
+    byte b = 10, c = -20;
+    short s = 1000, r = - 500;
+    int x = 10000, y =- 50000;
+    long l = 41000L, m = -55555L;
+    float f1 = 234.5f;
+    double d1 = 12.3;
+    char letterA = 'A';
+
+
+}
---
+++ Java SE/src/operators/Operator.java
@@ -0,0 +1,48 @@
+package operators;
+
+public class Operator {
+
+
+    public static void main(String[] args) {
+
+        //increment & decrement operator
+        int a=10;
+        int b=10;
+        System.out.println(a++ + ++a);//10+12=22
+        System.out.println(b++ + b++);//10+11=21
+
+        //inverting the value of a boolean
+        boolean c=true;
+        boolean d=false;
+        System.out.println(!c);//false (opposite of boolean value)
+        System.out.println(!d);//true
+
+        //shift operator
+        System.out.println(10<<3);//10*2^3=10*8=80 left shift
+        System.out.println(72>>3);//10/2^3=9 right shift
+
+        //logical vs bitwise operator
+         a=10;
+         b=5;
+         int e =20;
+        System.out.println("logical and");
+        System.out.println(a<b&&a++<e);//false && true = false
+        System.out.println(a);//10 because second condition is not checked
+        System.out.println(a<b&a++<e);//false && true = false
+        System.out.println(a);//11 because second condition is checked
+
+        System.out.println("logical operator");
+        System.out.println(a>b||a++<e);//true || true = true
+        System.out.println(a);//11 because second condition is not checked
+        System.out.println(a>b|a++<e);//true | true = true
+        System.out.println(a);//12 because second condition is checked
+
+
+        System.out.println("Assignment Operator");
+        short s1 = 25, s2 = 21;
+        s1+=s2;
+        //s1 = s1 + s2; compile time error because (s1+s2) return int
+        System.out.println(s1);
+
+    }
+}
