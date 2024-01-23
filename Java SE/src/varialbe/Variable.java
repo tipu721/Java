2fe30d085c449489bc885c90fe9e47f0ab413585
tipu721 <tsultan721@gmail.com>
2024-01-23 07:55:38 +0600

    varible type

---
+++ Java SE/src/varialbe/Variable.java
@@ -0,0 +1,16 @@
+package varialbe;
+
+public class Variable {
+
+    int data = 50; // instance variable related to object
+
+    static int m=100;//static variable
+    void method()
+    {
+        int n=90;//local variable
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
