edb2bb5af954efce285581c3c8ffd0f92f574315
tipu721 <tsultan721@gmail.com>
2024-01-26 10:43:10 +0600

    final keyword and initializer

--- Java SE/src/opp/polymorphism/FinalKeyword.java
+++ Java SE/src/opp/polymorphism/FinalKeyword.java
@@ -1,2 +1,26 @@
-package opp.polymorphism;public class FinalKeyword {
+package opp.polymorphism;
+
+final class C{ //can not inherite final class
+
+}
+
+
+class A{
+    final Integer a = 10;
+    Integer b = 20;
+    final void print(){
+        //a = 20; final variable can not changed
+        System.out.println("print method of class A");
+    }
+}
+class B extends A{
+    //void print() final method can not override
+    Integer b = 30;
+}
+
+public class FinalKeyword {
+    public static void main(String[] args) {
+        B b = new B();
+        b.print();
+    }
 }
--- Java SE/src/opp/polymorphism/InitializerBlock.java
+++ Java SE/src/opp/polymorphism/InitializerBlock.java
@@ -1,2 +1,23 @@
-package opp.polymorphism;public class InitializerBlock {
+package opp.polymorphism;
+
+class Parent1{
+
+    Parent1(){
+        System.out.println("Parent objetct created");
+    }
+
+}
+class Child1 extends Parent1{
+    Child1(){
+        System.out.println("Child object created");
+    }
+    {
+        System.out.println("Intializer block invocked");
+    }
+}
+
+public class InitializerBlock {
+    public static void main(String[] args) {
+        Child1 child = new Child1();
+    }
 }
