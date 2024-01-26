112e4e4111e1fee1005c4d93114964837c2b658e
tipu721 <tsultan721@gmail.com>
2024-01-26 08:20:35 +0600

    this keyword

--- Java SE/src/opp/ThisKeyword.java
+++ Java SE/src/opp/ThisKeyword.java
@@ -1,5 +1,6 @@
 package opp;
 
+// this use to point current object
 class A{
     int a = 10;
     void n(){
@@ -14,13 +15,60 @@ class A{
         System.out.println(this);
     }
 }
+
+//this constructor with parameter
+class B{
+
+    B(){
+        this(20);
+        System.out.println("class B called");
+       // this(20); must be 1st statement of constructor
+
+    }
+    B(int x){
+        System.out.println("B class called with "+x);
+    }
+}
+
+//this: to pass as argument in the constructor call
+class C{
+
+    C(C c){
+
+        System.out.println("constructor called with parameter");
+
+    }
+
+    C(){
+        C c = new C(this);
+    }
+
+    C getC(){
+       return this;
+    }
+
+}
 public class ThisKeyword {
 
 
     public static void main(String[] args) {
 
+        //this point current class object
         A a = new A();
         a.n();
+        //this to use currten class constructor
+        B b = new B();
+
+        //this: to pass as argument in the constructor call
+        C c = new C();
+
+        //return as a statement from the method
+
+        System.out.println("1: "+ c);
+        System.out.println(c.getC());
+
+
+
 
     }
 }
