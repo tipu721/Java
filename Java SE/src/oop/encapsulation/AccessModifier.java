d925c50fa97a46f62775038946f7431a6112c0b7
tipu721 <tsultan721@gmail.com>
2024-01-28 07:31:59 +0600

    access modifier

--- Java SE/src/oop/encapsulation/AccessModifier.java
+++ Java SE/src/oop/encapsulation/AccessModifier.java
@@ -6,10 +6,15 @@ class A{
     private int privateData = 5; //only accessable inside class
     protected int protectedData = 100; // accessable from inherited class also can be different package
     int defaultData = 30; // accessable from any same pacage class
+
+    void print(){
+        System.out.println(" A class method called to check accesmodifier");
+    }
     A(){
 
         System.out.println("A class constructor called");
     }
+
 
 
 }
@@ -18,11 +23,18 @@ class B extends A{
     B(){
         System.out.println("B class constructor called");
     }
+
+    @Override
+    void print() {
+        System.out.println("B class method called to check accesmodifier");
+    }
 }
 
 public class AccessModifier {
     public static void main(String[] args) {
         B b = new B();
+        PackageTest packageTest = new PackageTest();
+
 
     }
 }
