df95e1c741355ae4318cbbbc0973c8bba2f47c36
tipu721 <tsultan721@gmail.com>
2024-02-02 09:22:55 +0600

    documentation with javadoc command.

--- HelloJava.java
+++ HelloJava.java
@@ -1,6 +1,6 @@
-package mypack;
-class HelloJava{
-      
+/***  This is documentation for Java Hello world program */
+public class HelloJava{
+      /*** This is main method of java Hello World source file */
       public static void main(String[] args){
 	    System.out.println("Hello Java!");
       }
