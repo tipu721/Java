4398abebef2f5bf5068afb237a4e422c2f2c61cd
Tipu <tsultan721@gmail.com>
2024-03-08 16:12:20 +0600

    thread wiht lambda

--- Java SE/src/multi_threading/ThreadTest3.java
+++ Java SE/src/multi_threading/ThreadTest3.java
@@ -1,12 +1,35 @@
 package multi_threading;
 
-public class ThreadTest3 {
 
+public class ThreadTest3 implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("Thread is Running");
+    }
     public static void main(String[] args) {
+
+        // creating an object of the Thread class using the constructor Thread(String name)
         Thread t1 = new Thread("thread1");
 
+        //creating an object of the class Thread using Thread(Runnable r, String name)
+        Runnable r1 = new ThreadTest3();
+        Thread t2 = new Thread(r1, "My new thread");
         t1.start();
-        System.out.println(t1.getState());
+        t2.start();
+        //t1.start(); // can not start() thread multiple time
+        System.out.println(t2.getName());
+
+
+        Runnable r2 = ()-> {
+                for(int i=0; i<5; i++){
+                    System.out.println("Runnable Thead running");
+                }
+            };
+
+        Thread t3 = new Thread(r2);
+        t3.start();
     }
+
 
 }
---
+++ Java SE/src/multi_threading/ThreadWithLabmda.java
@@ -0,0 +1,18 @@
+package multi_threading;
+
+public class ThreadWithLabmda {
+
+    public static void main(String[] args) {
+        Runnable r1 = ()-> {
+            for(int i=0; i<5; i++){
+                System.out.println("Thread Running with Lambda");
+            }
+        };
+
+       Thread t1 = new Thread(r1);
+       t1.start();
+
+    }
+
+
+}
