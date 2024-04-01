e100a617b12133e825a7c1134504680cb57a784c
Tipu <tipu@divine-it.net>
2024-04-01 14:48:18 +0600

    messaging with socket

---
+++ Java SE/src/soket/MyClient.java
@@ -0,0 +1,35 @@
+package soket;
+
+import java.io.*;
+import java.net.Socket;
+
+public class MyClient {
+    public static void main(String[] args) {
+
+        try {
+            Socket s = new Socket("localhost", 6666);
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            DataInputStream din = new DataInputStream(s.getInputStream());
+
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String clientMessage = "", serverMessage = "";
+
+            while (!serverMessage.equals("Stop")){
+
+
+               clientMessage = br.readLine();
+               dout.writeUTF(clientMessage);
+               dout.flush();
+
+               serverMessage = din.readUTF();
+               System.out.println("Server Message :"+serverMessage);
+
+            }
+            dout.close();
+            s.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
